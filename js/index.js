5f210e423ac056d8f85ee84137496593b6225dbb
648800217 <648800217@qq.com>
2019-11-09 16:52:27 +0800

    douban

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var img = document.getElementById('#img');
+var code = document.getElementById('#code');
+
+img.onmouseover = function() {
+
+    // code.style.display = 'block'
+    console.log('1111');
+}
